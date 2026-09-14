8204631345c0a5b33d8d2788cb4831ee3c1f704d
Aswin Ram <aswinram40011@gmail.com>
2026-09-14 21:23:21 +0530

    Time: 909 ms (5.09%) | Memory: 370 MB (15.49%) - LeetSync

---
+++ 493-reverse-pairs/reverse-pairs.cpp
@@ -0,0 +1,71 @@
+class Solution {
+public:
+    void merge(vector<int>& arr, int i, int j, int mid){
+        if(i<j){
+            int a=i;
+            int b=mid+1;
+            int count=0;
+            vector<int> sorted;
+            
+            while(a<=mid && b<=j){
+                if(arr[a]<=arr[b]){
+                    sorted.push_back(arr[a]);
+                    a++;
+                }
+                else{
+                    sorted.push_back(arr[b]);
+                    b++;
+                }
+            }
+            while(a<=mid){
+                sorted.push_back(arr[a]);
+                a++;
+            }while(b<=j){
+                sorted.push_back(arr[b]);
+                b++;
+            }
+            
+            for(int k=0; k<sorted.size(); k++){
+                arr[i+k] = sorted[k];
+            }
+        }
+    }
+    int countIt(vector<int>& arr, int i, int j, int mid){
+        if(i<j){
+            int a=i;
+            int b=mid+1;
+            int count=0;
+            vector<int> sorted;
+            
+            while(a<=mid && b<=j){
+                if(arr[a]<=2*(long)arr[b]){
+                    sorted.push_back(arr[a]);
+                    a++;
+                }
+                else{
+                    count+=(mid-a+1);
+                    sorted.push_back(arr[b]);
+                    b++;
+                }
+            }
+            return count;
+        }
+        return 0;
+    }
+    int mergeSort(vector<int>& arr, int i, int j){
+        if(i<j){
+            int mid=(i+j)/2;
+            int l = mergeSort(arr,i,mid);
+            int r = mergeSort(arr,mid+1,j);
+            int c = countIt(arr,i,j,mid);
+            merge(arr,i,j,mid);
+            return l+r+c;
+        }
+        return 0;
+    }
+    int reversePairs(vector<int> &arr) {
+        // code here
+        int count=mergeSort(arr,0,arr.size()-1);
+        return count;
+    }
+};
